c2971c23212ff035162f1ba526d74be498d15753
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-10-30 12:03:43 +0400

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1>Hello from the About Page.</h1>
+</body>
+
+</html>
