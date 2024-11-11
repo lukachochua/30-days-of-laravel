b701ca8595075768e95066a74c965907138e115b
lukachochua <91949321+lukachochua@users.noreply.github.com>
2024-11-11 11:29:36 +0400

    day 19: routes reloaded - 6 essntial tips.

--- routes/web.php
+++ routes/web.php
@@ -1,87 +1,9 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 
-
-
-Route::get('/', function () {
-    return view('home');
-});
-
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3); // Solves N+1 Problem
-    return view('jobs.index', ['jobs' => $jobs]);
-});
-
-
-// Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Show
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
-});
-
-// Store
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-// Update
-Route::patch('/jobs/{id}', function ($id) {
-    // validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-    // authorize (on hold...)
-
-    // update the job and persist
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-    // Redirect
-    return redirect('/jobs/' . $job->id);
-});
-
-// Destroy
-Route::delete('/jobs/{id}', function ($id) {
-    // authorize (on hold...)
-    // delete the job
-    $job = Job::findOrFail($id);
-
-    $job->delete();
-
-    // redirect
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+Route::resource('jobs', JobController::class);
